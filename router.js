0a4e4b31b1290f8dab47a8d911e84374e4e5224a
markwy1 <dr.yongwang@hotmail.com>
2016-10-26 20:35:38 +0800

    Add files via upload

---
+++ router.js
@@ -0,0 +1,21 @@
+// yong wang
+// markwy@126.com
+// Date: 2016.10.24
+// Subject: nodejs exec, route
+// example is from: http://nodebeginner.org/index-zh-cn.html
+
+function route(handle, pathname, response, postData) {
+  console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    //返回是一个函数，字符串加个括号就可以是函数了？！确实灵活。
+    //为什么这里能引用到这个外部定义的函数?
+    handle[pathname](response, postData);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
+  }
+}
+
+exports.route = route;
