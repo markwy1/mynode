285ecca47b7c883ce2a946a8481db2dce378b307
markwy1 <dr.yongwang@hotmail.com>
2016-10-28 10:39:54 +0800

    f1 add upload file selector
    Changes to be committed:
    	modified:   requesthandlers.js

--- requesthandlers.js
+++ requesthandlers.js
@@ -7,6 +7,7 @@
 
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -17,7 +18,8 @@ function start(response, postData) {
     '</head>'+
     '<body>'+
     '<form action="/upload"  enctype="multipart/form-data" method="post">'+
-    '<input type="file" name="upload"> <input type="submit" value="Upload file" />'+
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -31,17 +33,28 @@ function start(response, postData) {
 
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + querystring.parse(postData).text);
-  response.end();
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    // 给上传文件硬性改名，为了演示。
+    fs.renameSync(files.upload.path, "/tmp/test.png")
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src=' /show' />")
+    //response.write("You've sent: " + querystring.parse(postData).text);
+    response.end();
+  })
 }
 
-function show(response, postData) {
+function show(response) {
   console.log("Request handler 'show' was called.");
-  // /tmp实际上是c:/tmp
+  // /tmp实际执行中是服务器上的c:/tmp（直接访问服务器C盘目录不太好吧）
+  // file应是读出的文件内容
   fs.readFile("/tmp/test.png", "binary", function(error, file) {
     if(error) {
       console.log("error...")
+      //这里的Content-Type是文本的error串
       response.writeHead(500, {"Content-Type": "text/plain"});
       response.write(error + "\n");
       response.end();
